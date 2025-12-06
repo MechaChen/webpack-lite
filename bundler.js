64a71467e14c1c6f321549b7612a8ee7096fb526
Benson Chen <tumlivein@gmail.com>
2025-12-06 20:48:49 +0800

    feat: create dependency graph from entry

--- bundler.js
+++ bundler.js
@@ -1,10 +1,53 @@
 const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+const parser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
 
 // parse a single file and extract its dependencies
 function createAsset(filename) {
   const content = fs.readFileSync(filename, 'utf-8');
 
-  console.log(content);
+  // get the AST with babel parser
+  const ast = parser.parse(content, {
+    sourceType: 'module',
+  });
+
+  const dependencies = [];
+
+  // traverse the AST and collect the dependencies
+  traverse(ast, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value);
+    },
+  });
+
+  return {
+    id: crypto.randomUUID(),
+    filename,
+    dependencies,
+  };
 }
 
-createAsset('./example/entry.js');
+function createGraph(entry) {
+  const mainAsset = createAsset(entry);
+  const queue = [mainAsset];
+
+  for (const asset of queue) {
+    const dirname = path.dirname(asset.filename);
+    asset.mapping = {};
+
+    asset.dependencies.forEach((relativeDependencyPath) => {
+      const absoluteDependencyPath = path.join(dirname, relativeDependencyPath);
+      const child = createAsset(absoluteDependencyPath);
+      asset.mapping[relativeDependencyPath] = child.id;
+      queue.push(child);
+    });
+  }
+
+  return queue;
+}
+
+const graph = createGraph('./example/entry.js');
+
+console.log(graph);
